4eef43e0992d27ef364ce65e0764344ef170c8e5
Ranju Sharma <sharmaranju684@gmail.com>
2026-08-06 22:34:19 +0545

    Add accounts app and authentication setup (#1)
    Co-authored-by: thespyler <alkatalankar@gmail.com>

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(
+        upload_to="profiles/",
+        blank=True,
+        null=True
+    )
+
---
+++ accounts/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "password"
+        ]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"]
+        )
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "bio",
+            "profile_picture",
+        ]
+        read_only_fields = ["id", "username", "email"]
